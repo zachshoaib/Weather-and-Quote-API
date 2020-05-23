d199a915001849d6df46f5a9b6c6af39eb3123b7
zach shoaib <zachshoaib@gmail.com>
2020-05-23 18:42:36 -0500

    removed duplicate code and combined functions

--- WEATHER/src/weather/MyWeatherBot.java
+++ WEATHER/src/weather/MyWeatherBot.java
@@ -48,20 +48,14 @@ public class MyWeatherBot extends PircBot {
         	{
         		sendMessage(channel, sender + " Try again using the command format \"weather <cityname>\" or \"weather <zipcode>\"");
         	}
-        	try {
-				//if command has numbers the user entered a zipcode
-				if(command[1].matches(".*\\d.*"))
-				{
-					getWeatherZipCode(channel, sender, command, wordcount);
-				}
-				//if no numbers then the user entered a city name
-				else{
-					getWeatherCityName(channel, sender, command, wordcount);
-				}
-				
-				
+			try
+			{
+				//build the url and get the request
+				getWeather(channel, sender, command, wordcount);
+			} 			
 			//send error message if city or zipcode is not known 
-			} catch (IOException e) {
+			catch (IOException e)
+			{
 				{
 					e.printStackTrace();
 					sendMessage(channel, sender + " The weather is not currently available you may have misspelled the city or zipcode");
@@ -90,17 +84,37 @@ public class MyWeatherBot extends PircBot {
     	sendMessage(channel, sender + ": To get a quote from breaking bad type \"quote\"");
     }
 
-	public void getWeatherZipCode(String channel, String sender, String[] command, int wordcount) throws IOException 
+	public void getWeather(String channel, String sender, String[] command, int wordcount) throws IOException 
 	{
 		//unique api token
-		String myAPItokenZ = "&APPID=3e013d807d3ca0ebef47ccc9207c20fb";
-		//url
-		String myAPIurlZ = "http://api.openweathermap.org/data/2.5/weather?zip=";
-		//zipcode from user
-		String userinputZ = command[1];
-		//full website
-		String weatherURL = myAPIurlZ + userinputZ + myAPItokenZ;
-		
+		String myAPItoken = "&APPID=3e013d807d3ca0ebef47ccc9207c20fb";
+
+		String myApiUrl = "";
+
+		//if input contains numbers the user has entered a zipcode
+		if(command[1].matches(".*\\d.*"))
+		{
+			myApiUrl = "http://api.openweathermap.org/data/2.5/weather?zip=";
+		}
+		//else it is a city name
+		else
+		{
+			myApiUrl = "http://api.openweathermap.org/data/2.5/weather?q=";
+		}
+
+		String userinput = command[1];
+
+		if(wordcount > 2)
+		{
+			userinput = command[1] + " " + command[2];
+		}
+		else
+		{
+			userinput = command[1];
+		}
+
+		String weatherURL = myApiUrl + userinput + myAPItoken;
+
 		//create weather object
 		URL url = new URL(weatherURL); 
 		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
@@ -111,42 +125,9 @@ public class MyWeatherBot extends PircBot {
 		//this function parses information from website
 		String temp = parseJsonFunction(result);
 		//send message to user containing temperature
-		sendMessage(channel, sender + ": The weather in " + userinputZ + " is " + temp + " degrees fahrenheit.");
-	}
-
-	public void getWeatherCityName(String channel, String sender, String[] command, int wordcount) throws IOException 
-	{
-		//unique api key
-		String myAPItoken = "&APPID=3e013d807d3ca0ebef47ccc9207c20fb";
-		//url
-		String myAPIurl = "http://api.openweathermap.org/data/2.5/weather?q=";
-		//user input that is a city
-		String userinput = command[1];
-		
-		//if 3 words inputted this means that the city has 2 words in it
-		if(wordcount > 2)
-		{
-			userinput = command[1] + " " + command[2];
-		}
-		else
-		{
-			userinput = command[1];
-		}
-		
-		//full website
-		String weatherURL = myAPIurl + userinput + myAPItoken;
-		//create weather object 
-		URL url = new URL(weatherURL); 
-		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-		conn.setRequestMethod("GET");
-		BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-		String result = rd.readLine();
-		//this function parses information from the website
-		String temp = parseJsonFunction(result);
-		//send message to user containing temperature 
 		sendMessage(channel, sender + ": The weather in " + userinput + " is " + temp + " degrees fahrenheit.");
-	}
 
+	}
     
     //this function parses the information using json tools to return the temperature
 	public static String parseJsonFunction(String json) 
@@ -157,7 +138,6 @@ public class MyWeatherBot extends PircBot {
 		//temp holds the temperature in kelvin
 		double temp = main.get("temp").getAsDouble();
 		//convert kelvin into fahrenheit
-		//temp = (9/5)*(temp - 273) + 32;
 		temp = temp * 1.8;
 		temp = temp - 459.67;
 		//round decimal to 2 positions
